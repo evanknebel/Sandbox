825edc48b77714a0f8799e9157a1bdd21dc778fc
Evan Knebel <s179060@students.aie.edu.au>
2018-09-12 12:44:20 -0700

    Wall collider fixes

--- Assets/August2018/Project/Animations/WallColliderExpander.cs
+++ Assets/August2018/Project/Animations/WallColliderExpander.cs
@@ -9,6 +9,10 @@ public class WallColliderExpander : MonoBehaviour
     BoxCollider animationBarrier;
 
     public bool bigboy;
+    public float tempValueCenter;
+    public float maxValueCenter;
+    public float tempValueSize;
+    public float maxValueSize;
 
 	// Use this for initialization
 	void Start ()
@@ -55,11 +59,21 @@ public class WallColliderExpander : MonoBehaviour
         {
             animationBarrier.center = new Vector3(0, 0, 0);
             animationBarrier.size = new Vector3(10, 0, 10);
+            tempValueCenter = 0f;
+            tempValueSize = 0f;
         }
         else
         {
-            animationBarrier.center = new Vector3(0, 0.5f, 0);
-            animationBarrier.size = new Vector3(10, 2.2f, 10);
+            if(tempValueCenter < maxValueCenter)
+            {
+                tempValueCenter += maxValueCenter * Time.deltaTime;
+            }
+            if (tempValueSize < maxValueSize)
+            {
+                tempValueSize += maxValueSize * Time.deltaTime;
+            }
+            animationBarrier.center = new Vector3(0, tempValueCenter, 0);
+            animationBarrier.size = new Vector3(10, tempValueSize, 10);
         }
 
 
