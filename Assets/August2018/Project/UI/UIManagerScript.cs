510a412d25b6eef76abbd8a7b14ec55199957439
Evan Knebel <s179060@students.aie.edu.au>
2018-09-26 12:45:18 -0700

    Added slider to flame color

--- Assets/August2018/Project/UI/UIManagerScript.cs
+++ Assets/August2018/Project/UI/UIManagerScript.cs
@@ -1,54 +1,56 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
 public class UIManagerScript : MonoBehaviour
 {
-    int fireColor;
+    int fireColorToggle;
+    float fireColorSlide;
 
     public ParticleSystem flame;
+    public Slider slider;
 
     Color myOrange;
+    Color myGreen;
 
-	// Use this for initialization
-	void Start ()
+    Color tempMyOrange;
+    Color tempMyGreen;
+
+    // Use this for initialization
+    void Start ()
     {
-        fireColor = 2;
+        fireColorToggle = 2;
 
-        //myOrange = new Color(248, 118, 0, 255);3
+        fireColorSlide = 0;
+
+        //myOrange = (248, 118, 0, 255)
         myOrange = new Color(0.97254902f, 0.4627451f, 0, 1);
+        //myGreen = (0, 255, 0, 255)
+        myGreen = new Color(0, 1, 0, 1);
     }
 
 
-    public void ColorChange()
+    public void ColorChangeToggle()
     {
-        if(fireColor == 1)
+        if (fireColorToggle == 1)
         {
             flame.startColor = myOrange;
-            fireColor++;
+            fireColorToggle++;
+            slider.value = 0;
         }
-        else if(fireColor == 2)
+        else if(fireColorToggle == 2)
         {
-            flame.startColor = Color.green;
-            fireColor--;
+            flame.startColor = myGreen;
+            fireColorToggle--;
+            slider.value = 1;
         }
     }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-    private void Update()
+    public void ColorChangeSlide()
     {
-        
+        tempMyOrange = new Color(myOrange.r * (1 - slider.value), myOrange.g * (1 - slider.value), myOrange.b * (1 - slider.value), myOrange.a * (1 - slider.value));
+        tempMyGreen = new Color(myGreen.r * slider.value, myGreen.g * slider.value, myGreen.b * slider.value, myGreen.a * slider.value);
+
+        flame.startColor = new Color (tempMyGreen.r + tempMyOrange.r, tempMyGreen.g + tempMyOrange.g, tempMyGreen.b + tempMyOrange.b, tempMyGreen.a + tempMyOrange.a);
     }
 }
